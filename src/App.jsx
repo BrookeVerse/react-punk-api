10af52e4d0d93887532a6d38b4e2d5573a8c4f05
BrookVerse <brookewaldorf90@gmail.com>
2022-09-16 12:27:10 +0100

    Deployed using HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 
 import Main from "./containers/Main/Main";
 import BeerInfo from "./components/BeerInfo/BeerInfo";
@@ -24,8 +24,8 @@ function App() {
     getBeers();
   }, []);
 
-  //handleCheckbox is checking each checkbox and when it is being checked it will run a functionn on 
-  //the array of beers, else recalling the API initial data. 
+  //handleCheckbox is checking each checkbox and when it is being checked it will run a functionn on
+  //the array of beers, else recalling the API initial data.
   const handleCheckbox = (button) => {
     if (button.target.checked) {
       if (button.target.value === "ABV") {
@@ -40,7 +40,7 @@ function App() {
     }
   };
 
-  //handleInput is checking what is being typed in the searchbox and if something is typed filtering 
+  //handleInput is checking what is being typed in the searchbox and if something is typed filtering
   //through the beer array to present a new list.
   const handleInput = (event) => {
     const cleanInput = event.target.value.toLowerCase();
@@ -56,24 +56,37 @@ function App() {
     }
   };
 
-//This area is rendering in the componenets and using routing to present different pages.
-//Also sending down props for the componenets to use including ternary operator. 
+  //This area is rendering in the componenets and using routing to present different pages.
+  //Also sending down props for the componenets to use including ternary operator.
   return (
-    <Router>
-      <div className="app">
-        <header className="app__header">
-          <h1 className="app__heading">Fancy A Brew?</h1>
-        </header>
+    <React.StrictMode>
+      <HashRouter>
+        <div className="app">
+          <header className="app__header">
+            <h1 className="app__heading">Fancy A Brew?</h1>
+          </header>
           <div className="app__content">
-            
-        <Routes>
-            <Route path="/beers/:beerId" element={<BeerInfo beerArr={searchWord.length < 1 ? beers : filterBeers}/>}></Route>
-            <Route path="/" element={<Main beerArr={searchWord.length < 1 ? beers : filterBeers} searchWord={searchWord} handleInput={handleInput} handleCheckbox={handleCheckbox}/>}> </Route>
-        </Routes>
+            <Routes>
+              <Route path="/beers/:beerId" element={<BeerInfo beerArr={searchWord.length < 1 ? beers : filterBeers} />}></Route>
+              <Route
+                path="/"
+                element={
+                  <Main
+                    beerArr={searchWord.length < 1 ? beers : filterBeers}
+                    searchWord={searchWord}
+                    handleInput={handleInput}
+                    handleCheckbox={handleCheckbox}
+                  />
+                }
+              >
+                {" "}
+              </Route>
+            </Routes>
           </div>
-      </div>
-    </Router>
+        </div>
+      </HashRouter>
+    </React.StrictMode>
   );
-} 
+}
 
 export default App;
