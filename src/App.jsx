cf86b43921937041f972fe036a64f85345c4f69f
BrookVerse <brookewaldorf90@gmail.com>
2022-09-16 13:51:27 +0100

    Deployment changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Main from "./containers/Main/Main";
 import BeerInfo from "./components/BeerInfo/BeerInfo";
